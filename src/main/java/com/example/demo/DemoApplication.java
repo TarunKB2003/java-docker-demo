c49dffbdf8653cdce1c31e551452891e3ddbd144
TarunBehuria_2003 <tarunkubehuria2003@gmail.com>
2026-04-18 00:16:10 +0530

    testing the ecr with error

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -10,7 +10,7 @@ public class DemoApplication {
 
     @GetMapping("/")
     public String home() {
-        return "Hello from Spring Boot Docker";
+        return "Hello from Spring Boot Docker and welcome";
     }
 
     public static void main(String[] args) {
