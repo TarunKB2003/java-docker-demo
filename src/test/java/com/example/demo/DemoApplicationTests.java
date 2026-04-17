d3e905adb25eb58e64b2be5610ce16254d76fc70
TarunBehuria_2003 <tarunkubehuria2003@gmail.com>
2026-04-18 02:01:05 +0530

    Test OIDC4

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
-
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -19,6 +18,6 @@ class DemoApplicationTests {
     void homeEndpointReturnsGreeting() throws Exception {
         mockMvc.perform(get("/"))
                 .andExpect(status().isOk())
-                .andExpect(content().string("Hello from Spring Boot Docker"));
+                .andExpect(content().string("Hello from Spring Boot Docker2"));
     }
 }
