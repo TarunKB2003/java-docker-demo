5feaacfca4b2a55b78b69aecbbaa4f19fb7b172a
TarunBehuria_2003 <tarunkubehuria2003@gmail.com>
2026-04-19 00:18:38 +0530

    testing dev

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -11,7 +11,7 @@ public class DemoApplication {
 
     @GetMapping("/")
     public String home() {
-        return "Hello from Spring Boot Docker and welcome to java.And hope you are alright..and will be g you guys.hello";
+        return "Hello from Spring Boot Docker and welcome to java.And hope you are alright..and willg you guys.hello";
     }
 
     public static void main(String[] args) {
