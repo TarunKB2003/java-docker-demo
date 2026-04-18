199fb7c20e3c5c8ddc6a591a0b18fff83813f957
TarunBehuria_2003 <tarunkubehuria2003@gmail.com>
2026-04-18 22:33:04 +0530

    added the dynamic ec2 containerization4

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -11,7 +11,7 @@ public class DemoApplication {
 
     @GetMapping("/")
     public String home() {
-        return "Hello from Spring Boot Docker and welcome to java.1 and hope you are alright..and will be meeting you guys.";
+        return "Hello from Spring Boot Docker and welcome to java.And hope you are alright..and will be meeting you guys.";
     }
 
     public static void main(String[] args) {
