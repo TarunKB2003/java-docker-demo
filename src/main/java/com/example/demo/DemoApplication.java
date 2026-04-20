1044f0a843dc358f5a3dce453b5b45d6eb4212d9
TarunBehuria_2003 <tarunkubehuria2003@gmail.com>
2026-04-20 11:15:53 +0530

    testing trust for prod role

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -11,7 +11,7 @@ public class DemoApplication {
 
     @GetMapping("/")
     public String home() {
-        return "Hello from Spring Boot Docker Application!!!";
+        return "Hello from Spring Boot Docker Application!!! and welcome onboard.";
     }
 
     public static void main(String[] args) {
