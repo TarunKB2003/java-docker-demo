95a33ee370e0981a11f6953b5ae1330419170cbb
TarunBehuria_2003 <tarunkubehuria2003@gmail.com>
2026-04-20 11:03:30 +0530

    testing uat

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -11,7 +11,7 @@ public class DemoApplication {
 
     @GetMapping("/")
     public String home() {
-        return "Hello from Spring Boot Docker and hope everyone is doing well!!";
+        return "Hello from Spring Boot Docker and hope everyone is doing well!! and meet you sooon.";
     }
 
     public static void main(String[] args) {
