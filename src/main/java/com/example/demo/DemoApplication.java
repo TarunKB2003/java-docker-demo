603755f8ecc157730e9d1e807b25166f5c3bd595
TarunBehuria_2003 <tarunkubehuria2003@gmail.com>
2026-04-22 12:39:02 +0530

    added the workflow diagram in prod

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -11,7 +11,7 @@ public class DemoApplication {
 
     @GetMapping("/")
     public String home() {
-        return "Hello from Spring Boot Docker Application. and welcome onboard, hope you enjoy the whole experience.";
+        return "Hello from Spring Boot Docker Application. and welcome onboard, hope you enjoy the whole experience!!.";
     }
 
     public static void main(String[] args) {
