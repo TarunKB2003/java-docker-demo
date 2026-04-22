e2a98870e8957090986a6e22ffee98b2c3b3b10e
TarunBehuria_2003 <tarunkubehuria2003@gmail.com>
2026-04-22 15:10:56 +0530

    testing the backend

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -4,12 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
 @RestController
 public class DemoApplication {
 
     @GetMapping("/")
+    @CrossOrigin(origins = "*")
     public String home() {
         return "Hello from Spring Boot Docker test file and welcome to java.And hope you are alright..and willing to help you guys.btw where is your location?";
     }
